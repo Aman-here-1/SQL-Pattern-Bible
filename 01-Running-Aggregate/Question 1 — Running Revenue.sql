fb1d4f224455a4617ef17c4dfc409b3fc998a372
Aman Chouhan <amanc7477@gmail.com>
2026-07-15 01:08:51 +0530

    files added

---
+++ 01-Running-Aggregate/Question 1 — Running Revenue.sql
@@ -0,0 +1,11 @@
+-- Chapter 1 - Running Aggregate
+-- Question 1 — Running Revenue
+-- Business Scenario
+
+-- You are working as a Product Analyst at Swiggy.
+
+-- The Finance team wants to track cumulative revenue generated over time.
+
+-- They have asked you to create a report showing how revenue grows with every new order.
+
+sel
--- 01-Running-Aggregate/create_table.sql
+++ 01-Running-Aggregate/create_table.sql
@@ -4,3 +4,12 @@ CREATE TABLE orders
     order_date DATE,
     amount INT
 );
+
+
+CREATE TABLE order (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    customer_id INT,
+    city VARCHAR(50),
+    revenue DECIMAL(10,2)
+);
